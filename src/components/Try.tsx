2831ccccd15d93ff0bda0764a3173c30354ca6d7
Prateek Kumar Singh <20cs01003@iitbbs.ac.in>
2024-01-09 15:22:02 +0530

    fixed some issue

--- src/components/Try.tsx
+++ src/components/Try.tsx
@@ -1,33 +1,40 @@
 /** @format */
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 function Try() {
-	//const [number, setNumber] = React.useState(99);
-	const [task, setTask] = React.useState('Hello');
+  //const [number, setNumber] = React.useState(99);
+  const [task, setTask] = React.useState("Hello");
+  
+  function handleClick(valueFromInput: string) {
+    setTask(valueFromInput);
+  }
 
-	function handleClick() {
-		// setTask();
-	}
-
-	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Enter your Todo"
-				value={task}
-				onChange={handleClick}
-				style={{
-					margin: '100px',
-					textAlign: 'center',
-					justifyContent: 'center',
-				}}
-			/>
-			<button onClick={handleClick} style={{ border: '2px solid blue' }}>
-				Click to add
-			</button>
-		</div>
-	);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your Todo"
+        value={task}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          handleClick(e.target.value);
+        }}
+        style={{
+          margin: "100px",
+          textAlign: "center",
+          justifyContent: "center",
+        }}
+      />
+      <button
+        onClick={() => {
+			     window.alert(task);
+        }}
+        style={{ border: "2px solid blue" }}
+      >
+        Click to add
+      </button>
+    </div>
+  );
 }
 
 export default Try;
